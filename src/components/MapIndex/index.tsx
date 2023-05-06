f851446bf7cc5585f1f6904473e0672e49d86588
lh <1767359165@qq.com>
2023-05-06 17:28:30 +0800

    特性: 🚀 地图加载

--- src/components/MapIndex/index.tsx
+++ src/components/MapIndex/index.tsx
@@ -1,43 +1,17 @@
-import { Map, View } from 'ol'
-import { Tile as TileLayer } from 'ol/layer'
+import 'ol/ol.css'
+import Map from 'ol/Map'
+import View from 'ol/View'
+import TileLayer from 'ol/layer/Tile'
+import OSM from 'ol/source/OSM'
 import { XYZ } from 'ol/source'
 import { defaults as defaultControls } from 'ol/control'
-import { Button } from 'antd'
-import { createRef, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import mapUrls from './mapUrls'
 import scssStyles from './index.scss'
 
 const MapIndex: React.FC = () => {
-  // 地图容器
-  const mapDom: any = createRef<HTMLDivElement>()
-  const [baseLayers, setBaseLayers] = useState<any>([])
-
+  const mapRef = useRef<HTMLDivElement>(null) as any
   useEffect(() => {
-    createdBaseLayer()
-    initMap()
-  }, [])
-
-  let map: any = null
-  const initMap = () => {
-    console.log(baseLayers)
-    map = new Map({
-      target: mapDom.current,
-      layers: baseLayers,
-      controls: defaultControls({
-        zoom: false, // 不显示放大放小按钮；
-        rotate: false, // 不显示指北针控件；
-        attribution: false // 不显示右下角的地图信息控件；
-      }).extend([]),
-      view: new View({
-        projection: 'EPSG:4326',
-        center: [120.5, 29.42],
-        zoom: 9,
-        maxZoom: 18,
-        minZoom: 6
-      })
-    })
-  }
-  const createdBaseLayer = () => {
     const vecLayer = new TileLayer({
       source: new XYZ({
         url: mapUrls['tian-vec']
@@ -54,10 +28,28 @@ const MapIndex: React.FC = () => {
         url: mapUrls['tian-roadLabel']
       })
     })
-    setBaseLayers([vecLayer, imgLayer, labelLayer])
-  }
+    const map: any = new Map({
+      target: mapRef.current,
+      layers: [vecLayer, imgLayer, labelLayer],
+      controls: defaultControls({
+        zoom: false, // 不显示放大放小按钮；
+        rotate: false, // 不显示指北针控件；
+        attribution: false // 不显示右下角的地图信息控件；
+      }).extend([]),
+      view: new View({
+        projection: 'EPSG:4326',
+        center: [120.5, 29.42],
+        zoom: 9,
+        maxZoom: 18,
+        minZoom: 6
+      })
+    })
+    return () => {
+      map.setTarget(null)
+    }
+  }, [])
   return (
-    <div id='map' ref={mapDom} className={scssStyles.mapBox}>
+    <div ref={mapRef} className={scssStyles.mapBox}>
       {/* <Button>切换底图</Button> */}
     </div>
   )
